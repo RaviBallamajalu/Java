9c9fabfe057b68cb7729b342a0f9cf2e0baa814d
Ravi Ballamajalu <ravi11051997@outlook.com>
2021-07-03 21:25:28 +0530

    Java Learning

---
+++ src/IfElse.java
@@ -0,0 +1,12 @@
+public class IfElse {
+    public static void main(String[] args) {
+        Integer number1=10;
+
+        if(number1>5){
+            System.out.println("number1 is greater");
+        }
+        else{
+            System.out.println("number1 is lesser");
+        }
+    }
+}
