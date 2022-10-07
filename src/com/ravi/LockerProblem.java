b26e1e42f1a87710e64fbebe46aba71d48171071
RaviBallamajalu <raviballamajalu@gmail.com>
2022-10-07 11:49:05 +0530

    LockerProblem

---
+++ src/com/ravi/LockerProblem.java
@@ -0,0 +1,19 @@
+package com.ravi;
+
+public class LockerProblem {
+    public static void main(String[] args) {
+        boolean [] b=new boolean[100];
+
+        for(int i=0;i<=b.length-1;i++){
+            for(int j=i;j<b.length-1;j++){
+                b[j]=true;
+            }
+        }
+
+        for (int i=0;i<b.length;i++){
+            System.out.println(b[i]+" "+i );
+        }
+
+
+    }
+}
